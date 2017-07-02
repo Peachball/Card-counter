9b8dd88ddf51da73236738c2161d8637f27056c3
Peachball <chenyexu@gmail.com>
2017-07-01 17:49:16 -0700

    Init

---
+++ index.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', function() {
+	var app = new Vue({
+		el: '#app',
+		data: {
+			'players': ['p1', 'p2', 'p3'],
+			'cards': function() {
+				var o = {'c': [], 'h': [], 's': [], 'd': []}
+				for(var i = 1; i <= 13; i++) {
+					var c = ['c', 'h', 's', 'd'];
+					for(var j = 0; j < c.length; j++) {
+						o[c[j]].push(i.toString());
+					}
+				}
+				return o;
+			}(),
+			'storage': {},
+		},
+		methods: {
+			pickCard: function(s, c) {
+				console.log(s, c, "picked");
+			}
+		}
+	});
+});
